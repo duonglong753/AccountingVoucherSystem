451d0d168312c8456fdd492621f5f7a12a82fa39
Duong Hung Long <duonglong753@gmail.com>
2025-05-29 14:46:27 +0700

    add new 3 DTOs responses

---
+++ Backend/Models/DTOs/Responses/AccountValidationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountingVoucherSystem.Models.DTOs.Responses
+{
+    public record AccountValidationResult(
+    bool IsValid,
+    string AccountCode,
+    string AccountName,
+    bool IsActive,
+    decimal CurrentBalance
+    );
+}
---
+++ Backend/Models/DTOs/Responses/VoucherValidationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountingVoucherSystem.Models.DTOs.Responses
+{
+    public record VoucherValidationResult(
+    int VoucherId,
+    bool IsTotalValid,
+    bool AreDatesValid
+    //List<VoucherDetailValidation> InvalidDetails
+    );
+}
